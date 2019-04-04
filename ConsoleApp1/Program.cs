99d3a276646f4895d1318eb60354010abc93c35b
Federico Perez <fede._perez@hotmail.com>
2019-04-04 01:07:08 -0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CalculaMaster;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculadora calc = new Calculadora();
+
+            Console.WriteLine("Calculator:\n");
+
+            Console.WriteLine("num1: ");
+            string num1 = Console.ReadLine();
+
+            Console.WriteLine("num2: ");
+            string num2 = Console.ReadLine();
+
+            Console.WriteLine("Result: " + calc.sumar(num1,num2));
+            Console.ReadKey();
+        }
+    }
+}
