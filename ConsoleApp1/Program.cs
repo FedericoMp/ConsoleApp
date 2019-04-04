6fcaafce3665ecd6fbfa3cfeb49307026ec54c37
Federico Perez <fede._perez@hotmail.com>
2019-04-04 17:35:30 -0300

    Se agrega otra clase, nuevos metodos.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -15,28 +15,43 @@ namespace ConsoleTest
             //instance of functions
             Calculadora Calc = new Calculadora();
             Printer PrintFrom = new Printer();
+            SuperStrings SuperStr = new SuperStrings();
 
-            Console.WriteLine("Program:\n");
+            Console.WriteLine("- ConsoleTest -");
+            Console.WriteLine($"at time: {Convert.ToString(SuperStr.RetornaFecha())}");
 
-            //take numbers
+            Console.WriteLine("---------------\n");
+
+            Console.WriteLine("Calcular:");
+
+            //Calculadora take numbers
             Console.WriteLine("num1: ");
             string num1 = Console.ReadLine();
             Console.WriteLine("num2: ");
             string num2 = Console.ReadLine();
 
+            //Calculadora
+            Console.WriteLine("\nSuma result: " + Calc.Sumar(num1, num2));
+            Console.WriteLine("Resta result: " + Calc.Restar(num1, num2) + "\n");
+
             Console.WriteLine("---------------\n");
 
-            //Printer()
+            //Print methods
             string resOut;
             PrintFrom.PrintOut(out resOut);
             string resRef = "";
             PrintFrom.PrintRef(ref resRef);
 
-            //Print all
+            Console.WriteLine("Imprime datos:");
+            //Printer
             Console.WriteLine(resOut);
             Console.WriteLine(resRef + "\n");
-            Console.WriteLine("Suma result: " + Calc.Sumar(num1,num2));
-            Console.WriteLine("Resta result: " + Calc.Restar(num1,num2) + "\n");
+
+            Console.WriteLine("---------------\n");
+
+            Console.WriteLine("Ingresar cadena de texto:");
+            string cadena = Console.ReadLine();
+            Console.WriteLine(SuperStr.MutaCadena(cadena));
 
             //End of program
             Console.ReadKey();
---
+++ ConsoleApp1/functions/SuperStrings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTest.functions
+{
+    class SuperStrings
+    {
+        //Acorta cadena de texto
+        public string MutaCadena(string cadena)
+        {
+            cadena = cadena.Substring(0, 4);
+            return cadena;
+        }
+
+        //Da la fecha y hora actual
+        public DateTime RetornaFecha()
+        {
+            DateTime fecha = DateTime.Now;
+            return fecha;
+        }
+    }
+}
