a9072810ff11bdf1119237376a17c8c4bce9e17f
Federico Perez <fede._perez@hotmail.com>
2019-04-08 10:34:13 -0300

    Cambio en el formato de datos, mejorado

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -18,20 +18,20 @@ namespace ConsoleTest
             SuperStrings SuperStr = new SuperStrings();
 
             Console.WriteLine("- ConsoleTest -");
-            Console.WriteLine($"{Convert.ToString(SuperStr.RetornaFecha())}");
+            Console.WriteLine($"{Convert.ToString(SuperStr.RetornaFecha())}\n");
 
             Console.WriteLine("---------------\n");
 
-            Console.WriteLine("Calcular:");
+            Console.WriteLine("Calcular:\n");
 
             //Calculadora take numbers
-            Console.WriteLine("num1: ");
+            Console.Write("num1: ");
             string num1 = Console.ReadLine();
-            Console.WriteLine("num2: ");
+            Console.Write("num2: ");
             string num2 = Console.ReadLine();
 
             //Calculadora
-            Console.WriteLine("\nSuma resultado: " + Calc.Sumar(num1, num2));
+            Console.WriteLine("\nSuma resultado: {0}", Calc.Sumar(num1, num2));
             Console.WriteLine("Resta resultado: " + Calc.Restar(num1, num2) + "\n");
 
             Console.WriteLine("---------------\n");
@@ -42,18 +42,21 @@ namespace ConsoleTest
             string resRef = "";
             PrintFrom.PrintRef(ref resRef);
 
-            Console.WriteLine("Imprime datos:");
+            Console.WriteLine("Imprime datos:\n");
             //Printer
             Console.WriteLine(resOut);
             Console.WriteLine(resRef + "\n");
 
             Console.WriteLine("---------------\n");
 
-            Console.WriteLine("Ingresar cadena de texto, imprime los 4 primeros caracteres::");
+            Console.WriteLine("Ingresar cadena de texto, imprime los 4 primeros caracteres:\n");
             string cadena = Console.ReadLine();
             Console.WriteLine(SuperStr.MutaCadena(cadena));
 
             //End of program
+            Console.WriteLine("\n---------------\n");
+            Console.WriteLine($"{Convert.ToString(SuperStr.RetornaFecha())}");
+            Console.WriteLine("- Fin -");
             Console.ReadKey();
         }
     }
